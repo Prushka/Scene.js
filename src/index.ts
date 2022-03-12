9a62977b910b5fb54cf34b1c0bfdf6de6b1aafb5
Dan Lyu <d.lyu@mail.utoronto.ca>
2022-03-12 10:00:33 -0500

    Update ConfigConstructor and prop list style

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import Position from "./props/Position";
-import {AnimationConfig, FrameAnimationConfig, PropConfig, PropType} from "./props/Props";
+import {AnimationConfig, FrameAnimationConfig, PropConfig, PropType, PropTypeIcons} from "./props/Props";
 
 export * as position from './props/Position'
 
 export class ConfigConstructor {
     protected ids: number = 0
     protected _props: Array<PropConfig> = []
+    protected currentFrame: number = 0
 
     public constructor() {
     }
@@ -14,19 +15,19 @@ export class ConfigConstructor {
         return this._props
     }
 
-    public addProp<T extends PropConfig>(propConfig: T,
-                                         propAnimations?: FrameAnimationConfig) {
-        propConfig.propId = propConfig.propId || this.ids
-        propConfig.frameAnimationConfig = propConfig.frameAnimationConfig || propAnimations
-        this.ids += 1
-        this._props.push(propConfig)
+    public addProp<T extends PropConfig>(...propConfigs: T[]) {
+        propConfigs.forEach(propConfig => {
+            propConfig.propId = propConfig.propId || this.ids
+            this.ids += 1
+            this._props.push(propConfig)
+        })
         return this
     }
 
     private getPropListHtml() {
         const lst: Array<string> = this.props.map(prop => {
             return `<div id='prop-list-${prop.propId}' class='prop__list__item'>
-                    <i class="bi-alarm prop__list__item__icon"></i><p>${prop.name}</p>
+                    <i class="${PropTypeIcons[prop.type].default.enabled} prop__list__item__icon"></i><p>${prop.name}</p>
                     </div>`
         })
         return lst.join('')
@@ -41,16 +42,20 @@ export class ConfigConstructor {
 }
 
 export function demo() {
-    const config: ConfigConstructor = new ConfigConstructor()
-    config.addProp({
-        name: "Light", iconColor: "#000000",
-        type: PropType.LIGHT,
-        colorTemperature: 5000,
-        frameAnimationConfig: {
-            1: {x: 200, y: 200, degree: 30},
-            2: {x: 20, y: 20, degree: 30, isOffset: false}
+    const getDemoLight = () => {
+        return {
+            name: "Hard Light",
+            type: PropType.LIGHT,
+            colorTemperature: 5000,
+            frameAnimationConfig: {
+                1: {x: 200, y: 200, degree: 30},
+                2: {x: 20, y: 20, degree: 30, isOffset: false}
+            }
         }
-    }).displayRoot("#scene")
+    }
+    const config: ConfigConstructor = new ConfigConstructor()
+    config.addProp(getDemoLight(), getDemoLight(), getDemoLight(),
+        getDemoLight(), getDemoLight()).displayRoot("#scene")
     console.log(config.props)
 
     return new Position(1, 1)
