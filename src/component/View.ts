4abb7f7e166572e62607c7f7245f0fff386351e5
Dan Lyu <d.lyu@mail.utoronto.ca>
2022-03-14 01:02:54 -0400

    Remove zoom with origin functionality...

--- src/component/View.ts
+++ src/component/View.ts
@@ -1,18 +1,10 @@
 import {SceneComponent} from "./Component";
-import {AnimationConfig, PositionConfig, PropTypeIcons} from "../props/Props";
+import {AnimationConfig, PositionConfig} from "../props/Props";
 import State from "../state/State";
-
-// Note I only implemented dragging (i.e., pan)
-// I didn't come up with this algorithm to pan plus zoom with mouse position
-// The algorithm part comes from https://stackoverflow.com/questions/38810242/set-canvas-zoom-scale-origin with modifications
-// TODO: This should be added to README
 
 export class View extends SceneComponent {
 
     mouse: PositionConfig
-    mouseR: PositionConfig
-    mouseS: PositionConfig
-    origin: PositionConfig
     dragging: boolean
 
     listen(): State<any>[] {
@@ -26,74 +18,49 @@ export class View extends SceneComponent {
     afterRender() {
         console.log("Reset")
         this.mouse = {x: 0, y: 0}
-        this.mouseR = {...this.mouse}
-        this.mouseS = {...this.mouse}
-        this.origin = {...this.mouse}
         this.dragging = false
-
-
-        const zoomedXInv = (number) => {
-            return Math.floor((number - this.mouseS.x) * (1 / this.context.viewportScale) + this.origin.x);
-        }
-
-        const zoomedYInv = (number) => {
-            return Math.floor((number - this.mouseS.y) * (1 / this.context.viewportScale) + this.origin.y);
-        }
-
-        const zoomedX = (number) => {
-            return Math.floor((number - this.origin.x) * this.context.viewportScale + this.mouseS.x);
-        }
-
-        const zoomedY = (number) => { // scale & origin Y
-            return Math.floor((number - this.origin.y) * this.context.viewportScale + this.mouseS.y);
-        }
 
         const stopDragging = (e) => {
             if (this.dragging) {
                 e.preventDefault()
                 this.dragging = false
-                const previous = this.context.viewportOffset
-                console.log(this.context.viewportScale)
-                this.context.viewportOffset = {
-                    x: zoomedX(this.context.viewportScale) + previous.x,
-                    y: zoomedY(this.context.viewportScale) + previous.y
-                }
                 $('.view-container').css('cursor', 'unset')
             }
         }
 
-        const regMouse = (e) => {
-            const c = $('.view-container')
-            this.mouse.x = e.clientX - c[0].getBoundingClientRect().left
-            this.mouse.y = e.clientY - c[0].getBoundingClientRect().top
-            let xx = this.mouseR.x
-            let yy = this.mouseR.y
-            this.mouseR.x = zoomedXInv(this.mouse.x);
-            this.mouseR.y = zoomedYInv(this.mouse.y);
-            return [xx, yy]
+        const render = () => {
+            $(`.view-svg`)
+                .attr("transform",
+                    `scale(${this.context.viewportScale} ${this.context.viewportScale}) translate(${this.context.viewportOffset.x}, ${this.context.viewportOffset.y})`);
         }
 
-        const render = () => {
-            const newX = zoomedX(this.context.viewportScale)
-            const newY = zoomedY(this.context.viewportScale)
-            $(`.view-svg`).attr("transform", `matrix(${this.context.viewportScale},0,0,${this.context.viewportScale},${newX}, ${newY})`);
+        const getMouseOffset = (e) => {
+            const c = $('.view-container')
+            return {x: e.clientX - c[0].getBoundingClientRect().left, y: e.clientY - c[0].getBoundingClientRect().top}
         }
 
         $('.view-container').on("mousedown", (e) => {
             e.preventDefault()
             this.dragging = true
-            $('.view-container').css('cursor', 'grabbing')
+            const c = $('.view-container')
+            this.mouse = getMouseOffset(e)
+            console.log(this.mouse)
+            c.css('cursor', 'grabbing')
         }).on("mousemove", (e) => {
             e.preventDefault()
-            const [xx, yy] = regMouse(e)
             if (this.dragging) {
-                this.origin.x -= this.mouseR.x - xx;
-                this.origin.y -= this.mouseR.y - yy;
-                this.mouseR.x = zoomedXInv(this.mouse.x);
-                this.mouseR.y = zoomedYInv(this.mouse.y);
+                const previous = this.context.viewportOffset
+                const previousMouse = {...this.mouse}
+                const currMouse = getMouseOffset(e)
+                this.mouse = currMouse
+                this.context.viewportOffset = {
+                    x: previous.x + currMouse.x - previousMouse.x,
+                    y: previous.y + currMouse.y - previousMouse.y,
+                }
+                console.log(this.context.viewportOffset)
                 render()
             }
-        }).on("mouseup mouseleave mouseout", (e) => {
+        }).on("mouseup mouseleave", (e) => {
             stopDragging(e)
         }).on('wheel', (e) => {
             e.preventDefault()
@@ -103,12 +70,6 @@ export class View extends SceneComponent {
                 this.context.viewportScale = Math.min(3, this.context.viewportScale * 1.02)
             } else { // zoom out
                 this.context.viewportScale = Math.max(0.4, this.context.viewportScale * (1 / 1.02))
-            }
-            this.origin = {...this.mouseR}
-            this.mouseS = {...this.mouse}
-            this.mouseR = {
-                x: zoomedXInv(this.mouse.x),
-                y: zoomedYInv(this.mouse.y)
             }
             render()
         })
@@ -125,7 +86,7 @@ export class View extends SceneComponent {
         const props = this.context.props.get()
         let s = props.map(prop => {
             const position: AnimationConfig = this.context.getPropPosition(prop)
-            return position && `<g class="view__prop" id="view-prop-${prop.propId}" transform="translate(${position.x}, ${position.y}) rotate(${position.degree * (Math.PI/180)})">
+            return position && `<g class="view__prop" id="view-prop-${prop.propId}" transform="translate(${position.x}, ${position.y}) rotate(${position.degree * (Math.PI / 180)})">
                         <text y="-5">test</text>
                         <path style="transform-origin: center center;" fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-7.5 3.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707V11.5z"/>
                     </g>`
