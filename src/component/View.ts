369b7db9fbcedf0ba56cf2c037e9ccc74388f1b6
Dan Lyu <d.lyu@mail.utoronto.ca>
2022-03-14 10:00:23 -0400

    Replace innerHTML with viewport group DOMs

--- src/component/View.ts
+++ src/component/View.ts
@@ -1,6 +1,7 @@
 import {SceneComponent} from "./Component";
 import {AnimationConfig, PositionConfig, PropTypeIcons} from "../props/Props";
 import State from "../state/State";
+import {createElement} from "../utils/Utils";
 
 export class View extends SceneComponent {
 
@@ -23,11 +24,14 @@ export class View extends SceneComponent {
         // scale(${this.context.viewportScale} ${this.context.viewportScale}) translate(${this.context.viewportOffset.x}, ${this.context.viewportOffset.y})
         $('.view__prop').each((index, element) => {
             const textElement = element.querySelector('text')
-            const parentOffset = {x: textElement.getBoundingClientRect().width, y: textElement.getBoundingClientRect().height}
+            const parentOffset = {
+                x: textElement.getBoundingClientRect().width,
+                y: textElement.getBoundingClientRect().height
+            }
             element.querySelectorAll('path').forEach(path => {
                 const pathWidth = path.getBoundingClientRect().width
                 const pathHeight = path.getBoundingClientRect().height
-                path.setAttribute("transform",`translate(${parentOffset.x/2}, 0)`)
+                // path.setAttribute("transform", `translate(${parentOffset.x / 2}, 0)`)
             })
 
         })
@@ -95,10 +99,19 @@ export class View extends SceneComponent {
         let s = props.map(prop => {
             const position: AnimationConfig = this.context.getPropPosition(prop)
             const selected = this.context.propSelected(prop)
-            return position && `<g class="view__prop ${selected ? 'view__prop--selected' : 'view__prop--not-selected'}" id="${this.context.getId(prop, 'view', 'prop')}" transform="translate(${position.x}, ${position.y}) rotate(${position.degree})">
-                        <text id="${this.context.getId(prop, 'view', 'prop', 'text')}" y="-7">${prop.name}</text>
-                        ${PropTypeIcons[prop.type][prop.iconStyle][this.context.isPropEnabled(prop) ? 'enabledSVG' : 'disabledSVG']}
-                    </g>`
+            const group = document.createElement("g")
+            group.classList.add("view__prop", selected ? 'view__prop--selected' : 'view__prop--not-selected')
+            group.id = this.context.getId(prop, 'view', 'prop')
+            group.setAttribute("transform", `translate(${position.x}, ${position.y}) rotate(${position.degree})`)
+            const text = document.createElement("text")
+            text.id = this.context.getId(prop, 'view', 'prop', 'text')
+            text.innerText = prop.name
+            text.setAttribute("y", "-7")
+            group.appendChild(text)
+            const path = createElement(PropTypeIcons[prop.type][prop.iconStyle][this.context.isPropEnabled(prop) ? 'enabledSVG' : 'disabledSVG'])
+            path.id = this.context.getId(prop, 'view', 'prop', 'icon')
+            group.appendChild(path)
+            return position && group.outerHTML
         })
         return `<svg class="view-svg" xmlns="http://www.w3.org/2000/svg">${s}</svg>`
     }
--- src/utils/Utils.ts
+++ src/utils/Utils.ts
@@ -5,3 +5,9 @@
 export function convertTypeToReadable(type: string): string {
     return `${type.charAt(0).toUpperCase()}${type.slice(1).toLowerCase()}`
 }
+
+export function createElement(html:string):Element {
+    const template = document.createElement('template')
+    template.innerHTML = html.trim()
+    return template.content.children[0];
+}
