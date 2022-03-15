bce5de2a677d390ad60d9b6c9ff4bf483c7035ac
Dan Lyu <d.lyu@mail.utoronto.ca>
2022-03-15 09:32:50 -0400

    Add PropTupleSet (sorted/order doesnt matter)

---
+++ src/utils/PropTupleSet.ts
@@ -0,0 +1,19 @@
+/*
+ * Copyright 2022 Dan Lyu.
+ */
+
+import {PropConfig} from "../props/Props";
+
+export default class PropTupleSet extends Set<string> {
+    private static sortIdTuple(id: [number, number]) {
+        return id[0] < id[1] ? [id[0], id[1]] : [id[1], id[0]]
+    }
+
+    addIdTuple(id: [number, number]) {
+        return super.add(PropTupleSet.sortIdTuple(id).join(','));
+    }
+
+    hasIdTuple(id: [number, number]) {
+        return super.has(PropTupleSet.sortIdTuple(id).join(','));
+    }
+}
