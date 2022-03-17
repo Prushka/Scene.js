fc9e74d0128016a7ebefac3d90afb979666470ae
Dan Lyu <d.lyu@mail.utoronto.ca>
2022-03-16 23:11:46 -0400

    Create helper functions forEachProp in View

--- src/component/View.ts
+++ src/component/View.ts
@@ -1,5 +1,15 @@
-import {CustomComponent, SceneComponent} from "./Component";
+import {SceneComponent} from "./Component";
+import {AnimationConfig, PropConfig} from "../props/Props";
 
-export default abstract class View extends SceneComponent{
+export default abstract class View extends SceneComponent {
     public abstract resetViewport(currentFrame?: number): void
+
+    public forEachPropWithPosition(f: (prop: PropConfig, position: AnimationConfig) => void) {
+        this.ctx.propsState.get().forEach(prop => {
+            const position = this.ctx.getPropPositionByCurrentFrame(prop)
+            if(position){
+                f(prop, position)
+            }
+        })
+    }
 }
