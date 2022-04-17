bc214c226587b773ef100c4db3501cd8b8b5feef
Dan Lyu <dan@muddy.ca>
2022-04-16 21:30:48 -0400

    Update server.ts

--- express/server.ts
+++ express/server.ts
@@ -8,7 +8,7 @@ const app = express();
 
 const path = require('path');
 
-app.use(express.static(path.join(__dirname, '/pub')))
+app.use(express.static(path.join(__dirname, '/build')))
 const port = process.env.PORT || 5000
 app.listen(port, () => {
     log(`Listening on port ${port}...`)
